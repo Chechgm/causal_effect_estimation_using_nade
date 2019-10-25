cd2eed7e18969f2528dd237b4d524cd96e51fe31
Sergio Garrido <sergio.gm.93@hotmail.com>
2019-10-25 17:00:29 +0200

    Added tanh

--- model.py
+++ model.py
@@ -10,12 +10,12 @@ class Kidney_net(nn.Module):
         super().__init__()
 
         # Hidden layers
-        self.hidden_L = nn.Linear(1, N_HU) # Kidney stone is not related to anything, so rece3ives constant as input of size 1
+        self.hidden_L = nn.Linear(1, N_HU, bias=False) # Kidney stone is not related to anything, so rece3ives constant as input of size 1
         self.hidden_T = nn.Linear(1, N_HU) # Treatment is affected by size of the stone so receives variable L of size 1 as input
         self.hidden_R = nn.Linear(2, N_HU) # Recovery is affected both by size and treatment, so receives two variables of size 1 as input
 
         # Output layers: all the variables in this case have only one parameter p as output
-        self.out_L = nn.Linear(N_HU, 1)
+        self.out_L = nn.Linear(N_HU, 1, bias=False)
         self.out_T = nn.Linear(N_HU, 1)
         self.out_R = nn.Linear(N_HU, 1)
 
@@ -23,13 +23,17 @@ class Kidney_net(nn.Module):
         const = torch.ones_like(x[:,0]) # Constant for the exogenous variables
 
         # We have to use the following "view" because of the input shape
-        h_L = F.relu(self.hidden_L(const.view(-1,1)))
-        h_T = F.relu(self.hidden_T(x[:,1].view(-1,1)))
-        h_R = F.relu(self.hidden_R(x[:,[0,1]].view(-1,2)))
+        #h_L = F.relu(self.hidden_L(const.view(-1,1)))
+        #h_T = F.relu(self.hidden_T(x[:,1].view(-1,1)))
+        #h_R = F.relu(self.hidden_R(x[:,[0,1]].view(-1,2)))
 
         #h_L = torch.sigmoid(self.hidden_L(const.view(-1,1)))
         #h_T = torch.sigmoid(self.hidden_T(x[:,1].view(-1,1)))
         #h_R = torch.sigmoid(self.hidden_R(x[:,[0,1]].view(-1,2)))
+
+        h_L = self.hidden_L(const.view(-1,1)).tanh()
+        h_T = self.hidden_T(x[:,1].view(-1,1)).tanh()
+        h_R = self.hidden_R(x[:,[0,1]].view(-1,2)).tanh()
 
         o_L = torch.sigmoid(self.out_L(h_L))
         o_T = torch.sigmoid(self.out_T(h_T))
