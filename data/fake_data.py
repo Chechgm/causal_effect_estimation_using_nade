dccf074e9f3353d1c8ac6d4ab7206d50b0cfaff9
Sergio Garrido <sergio.gm.93@hotmail.com>
2019-11-21 17:00:03 +0100

    Refactored some code and added the continuous recovery case neural network and negative log-likelihood

--- data/fake_data.py
+++ data/fake_data.py
@@ -2,7 +2,10 @@
 import numpy  as np
 import os.path
 
-def kidney_stones_data_generator(n=5000):
+###############################################################################
+# Binary data simulator and Simpson's paradox "Checker"
+###############################################################################
+def ks_binary_simulator(n=5000):
     """
     Creates data that resembles the kidney stone data set.
     Inputs:
@@ -29,25 +32,71 @@ def kidney_stones_data_generator(n=5000):
 
     return data
 
-data = kidney_stones_data_generator()
-print("Data created succesfully")
+def binary_data_check(data):
+    """
+    This function checks that the binary data generated follows the Simpson's paradox.
+    """
+    # Check that P(R=1 | B) > P(R=1 | A)
+    p_r_b = np.mean(data[data[:,1]==False, 2]) # P(R=1 | B)
+    p_r_a = np.mean(data[data[:,1]==True, 2])  # P(R=1 | A)
+    print("is P(R=1 | B) > P(R=1 | A)?", p_r_b > p_r_a)
+
+    # Check that, individually, P(R=1 | A, S) > P(R=1 | B, S)
+    #   and P(R=1 | A, L) > P(R=1 | B, L)
+
+    mask_l_a = ((data[:,1]==True) & (data[:,0]==True))
+    p_r_l_a  = np.mean(data[mask_l_a, 2])               # P(R=1 | A, L)
+    mask_l_b = ((data[:,1]==False) & (data[:,0]==True))
+    p_r_l_b  = np.mean(data[mask_l_b, 2])               # P(R=1 | B, L)
+    print("is P(R=1 | A, L) > P(R=1 | B, L)? ", p_r_l_a > p_r_l_b)
+
+    mask_s_a = ((data[:,1]==True) & (data[:,0]==False))
+    p_r_s_a  = np.mean(data[mask_s_a, 2])               # P(R=1 | A, S)
+    mask_s_b = ((data[:,1]==False) & (data[:,0]==False))
+    p_r_s_b  = np.mean(data[mask_s_b, 2])               # P(R=1 | B, S)
+    print("is P(R=1 | A, S) > P(R=1 | B, S)? ", p_r_s_a > p_r_s_b)
+
+# Create the data
+data = ks_binary_simulator()
+print("Binary data created succesfully")
+
+binary_data_check(data)
 
 # Saving them if not saved already
-if not os.path.exists('./kidney_data.npy'):
-    np.save('./kidney_data.npy', data)
-    print("Data saved succesfully")
+if not os.path.exists("./ks_binary_data.npy"):
+    np.save("./ks_binary_data.npy", data)
+    print("Binary data saved succesfully")
 
-# # Sanity checks (means of the recovery column)
-# np.mean(data[data[:,1]==False, 2]) # Treatment B should be greater than Treatment A
-# np.mean(data[data[:,1]==True, 2])  # Treatment A
-#
-#
-# mask_l_a = ((data[:,1]==True) & (data[:,0]==True))
-# np.mean(data[mask_l_a, 2])  # Treatment A and Large stones, should be greater than Treatment B and Large stones
-# mask_l_b = ((data[:,1]==False) & (data[:,0]==True))
-# np.mean(data[mask_l_b, 2])  # Treatment B and Large stones
-#
-# mask_s_a = ((data[:,1]==True) & (data[:,0]==False))
-# np.mean(data[mask_s_a, 2])  # Treatment A and small stones, should be greater than Treatment B and Small stones
-# mask_s_b = ((data[:,1]==False) & (data[:,0]==False))
-# np.mean(data[mask_s_b, 2])  # Treatment B and Small stones
+###############################################################################
+### Continuous recovery simulator
+###############################################################################
+def ks_cont_recovery_simulator(n=5000):
+    """
+    Creates data that resembles the kidney stone data set.
+    Inputs:
+    n Number of observations to be generated
+    Output:
+    A numpy array with three columns. Size of kidney stone (if 1 Large)
+        Treatment assigned (if 1 A), Recovery status (Normally distributed, depending on KS and T)
+    """
+    p_l = 343/(343+357) # Probability of large kidney stones
+    p_a_l = 263/343     # Probability of getting treatment a given small stones
+    p_b_l = 80/343      # Probability of getting treatment b given small stones
+
+    # Simulation
+    l = np.random.binomial(1, p_l, size=(n,1))           # Simulation of kidney stone size
+    a = np.random.binomial(1, l*p_a_l + (1-l)*(1-p_a_l)) # Simulation of treatment
+    r = np.random.normal(a*4 + np.exp(l), 2, size=(n,1)) # Simulation of recovery. The treatment effect is 4.
+
+    # Getting them together:
+    data = np.hstack((l, a, r))
+
+    return data
+
+data = ks_cont_recovery_simulator()
+print("Continuous treatment data created succesfully")
+
+# Saving them if not saved already
+if not os.path.exists("./ks_cont_rec_data.npy"):
+    np.save("./ks_cont_rec_data.npy", data)
+    print("Continuous treatment data saved succesfully")
--- model.py
+++ model.py
@@ -4,8 +4,12 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.distributions.bernoulli import Bernoulli
+from torch.distributions.normal import Normal
 
-class Kidney_net(nn.Module):
+###############################################################################
+### Binary kidney stones neural network and negative log-likelihood
+###############################################################################
+class binary_ks_net(nn.Module):
     def __init__(self, N_HU):
         super().__init__()
 
@@ -41,7 +45,7 @@ class Kidney_net(nn.Module):
 
         return o_L, o_T, o_R
 
-def neg_loglik(output, x):
+def binary_neg_loglik(output, x):
     """
     Compute the negative log-likelihood of our data given the output parameters and the data
     """
@@ -51,6 +55,64 @@ def neg_loglik(output, x):
     dist_L = Bernoulli(p_L)
     dist_T = Bernoulli(p_T)
     dist_R = Bernoulli(p_R)
+
+    # Estimate the log-likelihoods
+    NLL = -torch.mean(dist_L.log_prob(x[:,0].view(-1,1)) + dist_T.log_prob(x[:,1].view(-1,1)) + dist_R.log_prob(x[:,2].view(-1,1)))
+
+    return NLL
+
+###############################################################################
+### Continuous recovery neural network and negative log-likelihood
+###############################################################################
+class cont_rec_ks_net(nn.Module):
+    def __init__(self, N_HU):
+        super().__init__()
+
+        # Hidden layers
+        self.hidden_L = nn.Linear(1, N_HU, bias=False) # Kidney stone is not related to anything, so rece3ives constant as input of size 1
+        self.hidden_T = nn.Linear(1, N_HU) # Treatment is affected by size of the stone so receives variable L of size 1 as input
+        self.hidden_R = nn.Linear(2, N_HU) # Recovery is affected both by size and treatment, so receives two variables of size 1 as input
+
+        # Output layers: all the variables in this case have only one parameter p as output
+        self.out_L = nn.Linear(N_HU, 1, bias=False)
+        self.out_T = nn.Linear(N_HU, 1)
+        self.out_R_mu = nn.Linear(N_HU, 1)
+        self.out_R_log_sigma = nn.Linear(N_HU, 1)
+
+    def forward(self, x):
+        const = torch.ones_like(x[:,0]) # Constant for the exogenous variables
+
+        # We have to use the following "view" because of the input shape
+        #h_L = F.relu(self.hidden_L(const.view(-1,1)))
+        #h_T = F.relu(self.hidden_T(x[:,0].view(-1,1)))
+        #h_R = F.relu(self.hidden_R(x[:,[0,1]].view(-1,2)))
+
+        #h_L = torch.sigmoid(self.hidden_L(const.view(-1,1)))
+        #h_T = torch.sigmoid(self.hidden_T(x[:,0].view(-1,1)))
+        #h_R = torch.sigmoid(self.hidden_R(x[:,[0,1]].view(-1,2)))
+
+        h_L = self.hidden_L(const.view(-1,1)).tanh()
+        h_T = self.hidden_T(x[:,0].view(-1,1)).tanh()
+        h_R = self.hidden_R(x[:,[0,1]].view(-1,2)).tanh()
+
+        o_L = torch.sigmoid(self.out_L(h_L))
+        o_T = torch.sigmoid(self.out_T(h_T))
+        mu_R = torch.sigmoid(self.out_R_mu(h_R))
+        log_sigma_R = torch.sigmoid(self.out_R_log_sigma(h_R))
+
+        return o_L, o_T, o_R
+
+def cont_rec_neg_loglik(output, x):
+    """
+    Compute the negative log-likelihood of our data given the output parameters and the data
+    """
+    p_L, p_T, mu_R, log_sigma_R = output # Unpack the parameters of the distributions
+    sigma_R = torch.exp(log_sigma_R) # Convert the log scale
+
+    # Define the distributions
+    dist_L = Bernoulli(p_L)
+    dist_T = Bernoulli(p_T)
+    dist_R = Normal(mu_R, sigma_R)
 
     # Estimate the log-likelihoods
     NLL = -torch.mean(dist_L.log_prob(x[:,0].view(-1,1)) + dist_T.log_prob(x[:,1].view(-1,1)) + dist_R.log_prob(x[:,2].view(-1,1)))
